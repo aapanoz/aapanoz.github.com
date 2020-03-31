435876d4f4dae9ebb87ac91db0c142ffc863f0c9
aapanoz <62954299+aapanoz@users.noreply.github.com>
2020-03-31 14:17:23 -0300

    Update main.js
    Modificacion

--- Project_Boletines/javascript/main.js
+++ Project_Boletines/javascript/main.js
@@ -61,7 +61,7 @@ links.forEach(function(link){
     //Pone un delay en la salida para poder visualizar
     setTimeout(function(){
       //redirecciona
-      location.href = "/Project_Boletines";
+      location.href = "/Project_Boletines/";
     },600);
     //setInterval
 
